992a7d8abce3d7c53dd113e049eeab7efd88e0ee
paulamalisiamoreira <paula_m_moreira@estudante.sesisenai.org.br>
2024-05-14 09:03:50 -0300

    Colocar css e atividade 3 no console

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function calculoINSS() {
     const valorINSS = salario * 0.08;
     const salarioLiq = salario - valorINSS;
 
-    document.getElementById('nome').innerText = nome1;
+    document.getElementById('nomee').innerText = nome1;
     document.getElementById('salarioBrutoo').innerText = salario;
     document.getElementById('valorINSS').innerText = valorINSS;
     document.getElementById('salarioLiq').innerText = salarioLiq;
@@ -63,22 +63,38 @@ function calculoINSS() {
 
 function descontoINSSporSalario() {
     const salario2 = parseFloat(document.getElementById('salarioBruto').value);
+    const nome3 = document.getElementById('nome').value;
+    console.log("Vamos calcular seu desconto do INSS baseado no quanto você ganha!")
+
 
     if (salario2 < 1000.01 ) {
-        const salarioDescontado = salario2 * 0.08;
+        const valorDesconto = salario2 * 0.08;
+        console.log("Seu nome: " + nome3)
+        console.log("O valor do desconto é de: R$" + valorDesconto)
+        const salarioDescontado = salario2 - valorDesconto
+        console.log("O seu salário já descontado é de: R$" + salarioDescontado)
+
     }
     else if (salario2 > 1500 ){
-        const salarioDescontado = salario2 * 0.09;
+        const valorDesconto = salario2 * 0.09;
+        console.log("Seu nome: " + nome3)
+        console.log("O valor do desconto é de: R$" + valorDesconto)
+        const salarioDescontado = salario2 - valorDesconto
+        console.log("O seu salário já descontado é de: R$" + salarioDescontado)
+        
     }
     else {
-        const salarioDescontado = salario2 * 0.085;
+        const valorDesconto = salario2 * 0.085;
+        console.log("Seu nome: " + nome3)
+        console.log("O valor do desconto é de: R$" + valorDesconto)
+        const salarioDescontado = salario2 - valorDesconto
+        console.log("O seu salário já descontado é de: R$" + salarioDescontado)
     }
 
-    document.getElementById('').innerText = salario2;
-    document.getElementById('').innerText = salarioDescontado;
+    // document.getElementById('').innerText = salario2;
+    // document.getElementById('').innerText = salarioDescontado;
     // document.getElementById('').innerText = 
 
-   
 }
 
 
